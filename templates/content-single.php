78031b97b0520514b8c134c99483acf5bfd164cc
maxhall <maxwelljhall@gmail.com>
2016-03-23 16:19:28 +1100

    Continue styling
    Did a bunch of styling. None is final and all is intended to refine the
    approach.

--- templates/content-single.php
+++ templates/content-single.php
@@ -1,20 +1,47 @@
 <?php while (have_posts()) : the_post(); ?>
   <article <?php post_class(); ?>>
     <header>
-      <div>
-        <p>Identifier</p>
+      <div class="main-column">
+        <div class="reg-article__identifier">
+          <a href="#">Identifier</a>
+        </div>
       </div>
-      <h1 class="entry-title"><?php the_title(); ?></h1>
-      <div>
-        <p>Standfirst for this particularly strong article about Adele's eye contact. Yes, yes it is. Think piece, now.</p>
+      <div class="main-column">
+        <h1 class="reg-article__headline"><?php the_title(); ?></h1>
+      </div>
+      <div class="main-column">
+        <div class="reg-article__standfirst">
+          <p>Standfirst for this particularly strong article about Adele's eye contact. Yes, yes it is. Think piece, now.</p>
+        </div>
       </div>
       <?php get_template_part('templates/entry-meta'); ?>
-      <h4>Category<h4>
-      <h4>Social Buttons</h4>
-      <?php if ( has_post_thumbnail() ) {
-          the_post_thumbnail();
-      } ?>
-      <h4>Caption and Credit</h4>
+      <div class="main-column">
+        <div class="reg-article__single-column">
+          <div class="reg-article__category">
+            <h4>Category<h4>
+          </div>
+          <div class="reg-article__social-buttons">
+            <div class="reg-article__social-buttons__title">
+              <h4>Social Buttons</h4>
+            </div>
+            <div class="reg-article__social-button">
+              <a href="#">fb</a>
+            </div>
+            <div class="reg-article__social-button">
+              <a href="#">tw</a>
+            </div>
+            <div class="reg-article__social-button">
+              <a href="#">em</a>
+            </div>
+          </div>
+        </div>
+        <div class="reg-article__featured-image">
+          <?php if ( has_post_thumbnail() ) {
+              the_post_thumbnail();
+          } ?>
+          <h4>Caption and Credit</h4>
+        </div>
+      </div>
     </header>
     <div class="entry-content">
       <?php the_content(); ?>
--- templates/sidebar.php
+++ templates/sidebar.php
@@ -1,1 +1,53 @@
 <?php dynamic_sidebar('sidebar-primary'); ?>
+<div class="sidebar-recommended">
+  <h4 class="sidebar-recommended__title">
+    The Best Shit:
+  </h4>
+  <ul class="sidebar-recommended__list-items">
+    <li class="sidebar-recommended__list-item">
+      <a href="#">
+        <span class="sidebar-recommended__number">1</span>
+        <img src="http://filldunphy.com/80/80"></img>
+        <span class="sidebate-recommended__headline">
+          Political economy at risk from restructure.
+        </span>
+      </a>
+    </li>
+    <li class="sidebar-recommended__list-item">
+      <a href="#">
+        <span class="sidebar-recommended__number">2</span>
+        <img src="http://filldunphy.com/80/80"></img>
+        <span class="sidebate-recommended__headline">
+          USyd's hotline bling.
+        </span>
+      </a>
+    </li>
+    <li class="sidebar-recommended__list-item">
+      <a href="#">
+        <span class="sidebar-recommended__number">3</span>
+        <img src="http://filldunphy.com/80/80"></img>
+        <span class="sidebate-recommended__headline">
+          Where are the baby Ibis'?
+        </span>
+      </a>
+    </li>
+    <li class="sidebar-recommended__list-item">
+      <a href="#">
+        <span class="sidebar-recommended__number">4</span>
+        <img src="http://filldunphy.com/80/80"></img>
+        <span class="sidebate-recommended__headline">
+          World reaches peak meme.
+        </span>
+      </a>
+    </li>
+    <li class="sidebar-recommended__list-item">
+      <a href="#">
+        <span class="sidebar-recommended__number">5</span>
+        <img src="http://filldunphy.com/80/80"></img>
+        <span class="sidebate-recommended__headline">
+          Twitter is weird.
+        </span>
+      </a>
+    </li>
+  </ul>
+</div>
