2849ee98989a247cdca40a723065437bcb83e7e4
maxhall <maxwelljhall@gmail.com>
2016-04-06 14:44:04 +1000

    Bootstrap Menu Walker
    Initial commit implementing the custom bootstrap menu walker and adding
    a search form.

--- templates/header.php
+++ templates/header.php
@@ -1,14 +1,37 @@
 <header class="banner">
-  <div class="container">
-    <a class="brand" href="<?= esc_url(home_url('/')); ?>"><?php bloginfo('name'); ?></a>
-    <nav class="nav-primary">
-      <?php
-      if (has_nav_menu('primary_navigation')) :
-        wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav']);
-      endif;
-      ?>
+    <nav class="navbar navbar-default" role="navigation">
+      <div class="container">
+        <!-- Brand and toggle get grouped for better mobile display -->
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="<?= esc_url(home_url('/')); ?>">
+            <img src="http://honisoit.com/wp-content/uploads/2016/02/Honilogo20161.png" width="135px" height="30px"></img>
+          </a>
+
+        </div>
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+        <?php
+            wp_nav_menu( array(
+                'menu'              => 'primary_navigation',
+                'theme_location'    => 'primary_navigation',
+                'depth'             => 2,
+                'menu_class'        => 'nav navbar-nav',
+                'fallback_cb'       => 'wp_bootstrap_navwalker::fallback',
+                'walker'            => new wp_bootstrap_navwalker())
+            );
+        ?>
+        <form class="navbar-form navbar-right" role="search">
+          <div class="form-group">
+            <input type="text" class="form-control" placeholder="Search">
+          </div>
+          <button type="submit" class="btn btn-default">Submit</button>
+        </form>
+        </div>
+      </div>
     </nav>
-    <h1>This is where the Logo and Menu go</h1>
-    <hr>
-  </div>
 </header>
