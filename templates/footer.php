7ddf95b9415398cf44c715b7bbc2771126675e3e
maxhall <maxwelljhall@gmail.com>
2016-06-20 20:50:38 +1000

    Initial Footer Markup

--- templates/footer.php
+++ templates/footer.php
@@ -1,7 +1,14 @@
-<footer class="content-info">
-  <div class="container">
-    <hr>
+<footer class="footer">
+  <hr>
+  </div>
+  <div class="footer__social">
+  </div>
+  <div class="footer__list">
+  </div>
+  <div class="footer__list">
     <?php dynamic_sidebar('sidebar-footer'); ?>
+  </div>
+  <div class="footer__small-text">
     <span><?php
       $options = get_option( 'global_options' );
       $option = $options['copyright'];
