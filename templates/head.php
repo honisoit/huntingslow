2169e93450e0efad3220630ec66cf51195154df6
slangers <samlouiselang@gmail.com>
2016-07-06 16:37:56 +1000

    baseline grid attempt
    in theory it's happening. there are a couple of elements I haven't
    styled yet that don't have it applied, but
    headline/standfirst/body/bylines do. It's not doing great stuff for
    bylines. Going to look into whether there's a better way to do it w/ the
    modular scale

--- templates/head.php
+++ templates/head.php
@@ -2,6 +2,6 @@
   <meta charset="utf-8">
   <meta http-equiv="x-ua-compatible" content="ie=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link href='https://fonts.googleapis.com/css?family=Open+Sans:300,700|Roboto:300|Lato:300|Source+Sans+Pro:300|Roboto+Mono:300|Cutive+Mono|Lora:400,400italic|Merriweather:700' rel='stylesheet' type='text/css'>
+  <link href='https://fonts.googleapis.com/css?family=Open+Sans:300,700|Signika+Negative:300,700|Roboto:300,700|Lato:300|Source+Sans+Pro:300|Roboto+Mono:300|Cutive+Mono|Lora:400,400italic|Merriweather:700' rel='stylesheet' type='text/css'>
   <?php wp_head(); ?>
 </head>
