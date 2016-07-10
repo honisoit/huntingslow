5ed58efaa8873fd97f5d3b03e81b6594792591ce
slangers <samlouiselang@gmail.com>
2016-07-10 21:22:05 +1000

    styled share
    changed markup in share fragment so that only the circle is a link.
    added a hover effect to the circle.
    
    at the moment the SVGs aren't perfectly centred when the breakpoint size
    changes. I'll fix this l8r.
    
    getting the text to centre vertically is also a little hacky, will also
    return to later

--- templates/share.php
+++ templates/share.php
@@ -1,18 +1,14 @@
 <ul class="share__buttons">
   <li class="share__button">
-    <a href="#">
-      <span class="share__logo share__logo--fb">
-        <svg width="32" height="32" viewBox="-2 -2 32 32"><path d="M17.9 14h-3v8H12v-8h-2v-2.9h2V8.7C12 6.8 13.1 5 16 5c1.2 0 2 .1 2 .1v3h-1.8c-1 0-1.2.5-1.2 1.3v1.8h3l-.1 2.8z"></path></svg>
-      </span>
+      <a href="#" class="share__logo share__logo--fb">
+        <svg width="32" height="32" viewBox="-3 -6 30 35"><path d="M17.9 14h-3v8H12v-8h-2v-2.9h2V8.7C12 6.8 13.1 5 16 5c1.2 0 2 .1 2 .1v3h-1.8c-1 0-1.2.5-1.2 1.3v1.8h3l-.1 2.8z"></path></svg>
+      </a>
       <span class="share__total share__total--fb">42</span>
-    </a>
   </li>
   <li class="share__button">
-    <a href="#">
-      <span class="share__logo share__logo--tw">
-        <svg width="32" height="32" viewBox="-2 -2 32 32"><path d="M21.3 10.5v.5c0 4.7-3.5 10.1-9.9 10.1-2 0-3.8-.6-5.3-1.6.3 0 .6.1.8.1 1.6 0 3.1-.6 4.3-1.5-1.5 0-2.8-1-3.3-2.4.2 0 .4.1.7.1l.9-.1c-1.6-.3-2.8-1.8-2.8-3.5.5.3 1 .4 1.6.4-.9-.6-1.6-1.7-1.6-2.9 0-.6.2-1.3.5-1.8 1.7 2.1 4.3 3.6 7.2 3.7-.1-.3-.1-.5-.1-.8 0-2 1.6-3.5 3.5-3.5 1 0 1.9.4 2.5 1.1.8-.1 1.5-.4 2.2-.8-.3.8-.8 1.5-1.5 1.9.7-.1 1.4-.3 2-.5-.4.4-1 1-1.7 1.5z"></path></svg>
-      </span>
-      <span class="share__total share__total--tw">42</span>
+    <a href="#" class="share__logo share__logo--tw">
+        <svg width="32" height="32" viewBox="-3 -6 30 35"><path d="M21.3 10.5v.5c0 4.7-3.5 10.1-9.9 10.1-2 0-3.8-.6-5.3-1.6.3 0 .6.1.8.1 1.6 0 3.1-.6 4.3-1.5-1.5 0-2.8-1-3.3-2.4.2 0 .4.1.7.1l.9-.1c-1.6-.3-2.8-1.8-2.8-3.5.5.3 1 .4 1.6.4-.9-.6-1.6-1.7-1.6-2.9 0-.6.2-1.3.5-1.8 1.7 2.1 4.3 3.6 7.2 3.7-.1-.3-.1-.5-.1-.8 0-2 1.6-3.5 3.5-3.5 1 0 1.9.4 2.5 1.1.8-.1 1.5-.4 2.2-.8-.3.8-.8 1.5-1.5 1.9.7-.1 1.4-.3 2-.5-.4.4-1 1-1.7 1.5z"></path></svg>
     </a>
+      <span class="share__total share__total--tw">42</span>
   </li>
 </ul>
