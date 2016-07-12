9a215133dea67b4a16e22e39c7ef3ad26f52fcbc
maxhall <maxwelljhall@gmail.com>
2016-07-12 23:01:28 +1000

    Information page styling
    Basic set up and initial structural style for the information pages
    (About, contact, advertising etc)

--- page.php
+++ page.php
@@ -1,4 +1,4 @@
+<?php get_template_part('templates/large-header'); ?>
 <?php while (have_posts()) : the_post(); ?>
-  <?php get_template_part('templates/page', 'header'); ?>
   <?php get_template_part('templates/content', 'page'); ?>
 <?php endwhile; ?>
--- templates/content-page.php
+++ templates/content-page.php
@@ -1,2 +1,20 @@
-<?php the_content(); ?>
-<?php wp_link_pages(['before' => '<nav class="page-nav"><p>' . __('Pages:', 'sage'), 'after' => '</p></nav>']); ?>
+<div class="content-wrapper">
+  <div class="page">
+    <aside class="page__links-column">
+      <?php
+          wp_nav_menu( array(
+              'menu'              => 'footer_pages',
+              'theme_location'    => 'footer_pages',
+              'walker'            => new Footer_Walker,
+              'items_wrap'        => '<ul class="page__list">%3$s</ul>'
+            )
+          );
+      ?>
+    </aside>
+    <article class="page__content-column">
+      <?php get_template_part('templates/page', 'header'); ?>
+      <?php the_content(); ?>
+      <?php wp_link_pages(['before' => '<nav class="page-nav"><p>' . __('Pages:', 'sage'), 'after' => '</p></nav>']); ?>
+    </article>
+  </div>
+</div>
