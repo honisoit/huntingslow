70c3d08b90347ba3afa9e0b899f74ca94b4f390e
maxhall <maxwelljhall@gmail.com>
2016-07-12 22:30:42 +1000

    Structural tidying and article summaries
    - Rejigged higher up markup to allow the right templates to be called
    at the right time (e.g. the header on the front page)
    - Added markup for the article summary pane that will appear on all
    archive pages
    - Removed the unnecessary entry-meta template

--- base.php
+++ base.php
@@ -14,9 +14,7 @@ use Roots\Sage\Wrapper;
         <?php _e('You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.', 'sage'); ?>
       </div>
     <![endif]-->
-    <div class="wrap container" role="document">
-      <?php include Wrapper\template_path(); ?>
-    </div><!-- /.wrap -->
+    <?php include Wrapper\template_path(); ?>
     <?php
       do_action('get_footer');
       get_template_part('templates/footer');
--- templates/content.php
+++ templates/content.php
@@ -1,9 +1,29 @@
-<article <?php post_class(); ?>>
-  <header>
-    <h2 class="entry-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
-    <?php get_template_part('templates/entry-meta'); ?>
-  </header>
-  <div class="entry-summary">
-    <?php the_excerpt(); ?>
+<article class="article-summary">
+  <div class="article-summary__image">
+    <?php if ( has_post_thumbnail() ) {
+        the_post_thumbnail();
+    } ?>
+  </div>
+  <div class="article-summary__copy">
+    <header>
+      <span class="article-summary__category"><?php the_category(','); ?> — </span>
+      <span class="article-summary__primary-tag">
+        <?php
+          $primary_tag_id = get_post_meta( get_the_id(), 'primary_tag', true );
+          $primary_tag_array = get_term_by( 'id', $primary_tag_id, 'post_tag', ARRAY_A);
+          echo ucwords($primary_tag_array['name']);
+        ?>
+      </span>
+      <h2 class="article-summary__headline"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+      <span>
+        <time class="" datetime="<?= get_post_time('c', true); ?>"><?= get_the_date(); ?></time>
+      </span>
+      <span>
+        <?= __('By', 'sage'); ?> <a href="<?= get_author_posts_url(get_the_author_meta('ID')); ?>" rel="author" class="fn"><?= get_the_author(); ?></a>
+      </span>
+    </header>
+    <div class="article-summary__excerpt">
+      <?php the_excerpt(); ?>
+    </div>
   </div>
 </article>
