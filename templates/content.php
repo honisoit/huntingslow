586843385d810b90d6983a74438eaf69ce4c094c
maxhall <maxwelljhall@gmail.com>
2016-07-21 16:48:14 +1000

    Improve category and tag display
    - Spacing between categories
    - the topic tag and its “//“ prefix is only displayed if it exists (y)

--- templates/content.php
+++ templates/content.php
@@ -6,12 +6,14 @@
   </div>
   <div class="article-summary__copy">
     <header>
-      <span class="article-summary__category"><?php the_category(','); ?> — </span>
+      <span class="article-summary__category"><?php the_category(', '); ?></span>
       <span class="article-summary__primary-tag">
         <?php
           $primary_tag_id = get_post_meta( get_the_id(), 'primary_tag', true );
           $primary_tag_array = get_term_by( 'id', $primary_tag_id, 'post_tag', ARRAY_A);
-          echo ucwords($primary_tag_array['name']);
+          if ($primary_tag_array['name']) {
+            echo '// ' . ucwords($primary_tag_array['name']);
+          }
         ?>
       </span>
       <h4 class="article-summary__headline"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h4>
