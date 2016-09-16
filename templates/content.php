3ff1aa810d52c48d6cafd956cdd25a1b5f130479
slangers <samlouiselang@gmail.com>
2016-09-16 11:12:07 +1000

    hopefully the end of link styling
    as of this commit (some of this was achieved in the previous commit):
    
    - most (apparatus/headline) links do nothing re: hover effects etc
    - links in the header/footer display a $brand-primary underline on hover
    - links in the body of articles have their underline change color on
    hover
    
    all up for discussion, let me know if I've missed a type of link

--- templates/content.php
+++ templates/content.php
@@ -18,7 +18,7 @@
     <span>
       <time class="article-summary__date" datetime="<?= get_post_time('c', true); ?>"><?= get_the_date(); ?></time>
     </span>
-    <span class="article-summary__byline">|
+    <span class="article-summary__byline">
       <?php
         if ( function_exists( 'coauthors_posts_links' ) ) {
           coauthors_posts_links();
