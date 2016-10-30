82f55d75dd5e8a4b597027d3ce1a21b69a0b346a
maxhall <maxwelljhall@gmail.com>
2016-10-30 15:07:12 +1100

    Simply archive template hierarchy
    Removes individual search and archive templates. Both now use the
    index.php layout to display search, author archive, month archive, etc.
    results

--- index.php
+++ index.php
@@ -1,17 +1,25 @@
+<?php get_template_part('templates/large-header'); ?>
+
 <div class="content-wrapper">
-  <?php get_template_part('templates/large-header'); ?>
-  <?php get_template_part('templates/page', 'header'); ?>
+  <div class="archive">
+    <div class="archive__main-column">
+      <?php get_template_part('templates/page', 'header'); ?>
 
-  <?php if (!have_posts()) : ?>
-    <div class="alert alert-warning">
-      <?php _e('Sorry, no results were found.', 'sage'); ?>
+      <?php if (!have_posts()) : ?>
+        <div class="alert alert-warning">
+          <?php _e('Sorry, no results were found.', 'sage'); ?>
+        </div>
+        <?php get_search_form(); ?>
+      <?php endif; ?>
+
+      <?php while (have_posts()) : the_post(); ?>
+        <?php get_template_part('templates/content', get_post_type() != 'post' ? get_post_type() : get_post_format()); ?>
+      <?php endwhile; ?>
+
+      <?php the_posts_pagination(); ?>
     </div>
-    <?php get_search_form(); ?>
-  <?php endif; ?>
-
-  <?php while (have_posts()) : the_post(); ?>
-    <?php get_template_part('templates/content', get_post_type() != 'post' ? get_post_type() : get_post_format()); ?>
-  <?php endwhile; ?>
-
-  <?php the_posts_pagination(); ?>
+    <aside class="archive__sidebar">
+      <?php dynamic_sidebar('archive-page-sidebar'); ?>
+    </aside>
+  </div>
 </div>
