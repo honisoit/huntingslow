3171b22e1e27615ae239289c0d6b46bd83cde3f5
maxhall <maxwelljhall@gmail.com>
2016-11-11 15:55:05 +1100

    Markup and initial styles for page

--- templates/content-page.php
+++ templates/content-page.php
@@ -1,5 +1,10 @@
 <div class="content-wrapper">
   <div class="page">
+    <article class="page__content-column">
+      <?php get_template_part('templates/page', 'header'); ?>
+      <?php the_content(); ?>
+      <?php wp_link_pages(['before' => '<nav class="page-nav"><p>' . __('Pages:', 'sage'), 'after' => '</p></nav>']); ?>
+    </article>
     <aside class="page__links-column">
       <?php
           wp_nav_menu( array(
@@ -11,10 +16,5 @@
           );
       ?>
     </aside>
-    <article class="page__content-column">
-      <?php get_template_part('templates/page', 'header'); ?>
-      <?php the_content(); ?>
-      <?php wp_link_pages(['before' => '<nav class="page-nav"><p>' . __('Pages:', 'sage'), 'after' => '</p></nav>']); ?>
-    </article>
   </div>
 </div>
