e3271a5e9ffe78506bb7e353f191bd52727937b0
maxhall <maxwelljhall@gmail.com>
2016-11-17 16:44:14 +1100

    Resolved search form conflicts

--- searchform.php
+++ searchform.php
@@ -1,6 +1,6 @@
 <form role="search" method="get" class="search-form" action="<?php esc_url( home_url( '/' ) ); ?>">
   <label class="search-form__label">
-    <span class="screen-reader-text">' . _x( 'Search for:', 'label' ) . '</span>
+    <span class="screen-reader-text">Search for:</span>
     <input type="search" class="search-form__field" placeholder="search" value="" name="s" />
   </label>
   <input type="submit" class="search-form__submit" value="X"/>
