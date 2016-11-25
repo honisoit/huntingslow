de3260972339946285c4f30d13ec8df6eaa3a8e3
maxhall <maxwelljhall@gmail.com>
2016-11-25 16:49:02 +1100

    Creates separate template fragment for primary tag

--- templates/category-line.php
+++ templates/category-line.php
@@ -1,13 +1,5 @@
 <span class="category-line__category"><?php the_category(', '); ?></span>
 <span class="category-line__primary-tag">
-  <?php
-    $primary_tag_id = get_post_meta( get_the_id(), 'primary_tag', true );
-    $primary_tag_array = get_term_by( 'id', $primary_tag_id, 'post_tag', ARRAY_A);
-    $primary_tag = ucwords($primary_tag_array['name']);
-    $primary_tag_url = '/tag/' . $primary_tag_array['slug'];
-
-    if ($primary_tag) : ?>
-      <span>// </span>
-      <a href="<?php echo esc_url( $primary_tag_url ); ?>"><?php echo esc_html( $primary_tag ); ?></a>
-    <?php endif; ?>
+  <span>// </span>
+  <?php get_template_part('templates/primary-tag'); ?>
 </span>
---
+++ templates/primary-tag.php
@@ -0,0 +1,9 @@
+<?php
+  $primary_tag_id = get_post_meta( get_the_id(), 'primary_tag', true );
+  $primary_tag_array = get_term_by( 'id', $primary_tag_id, 'post_tag', ARRAY_A);
+  $primary_tag = ucwords($primary_tag_array['name']);
+  $primary_tag_url = '/tag/' . $primary_tag_array['slug'];
+
+  if ($primary_tag) : ?>
+    <a href="<?php echo esc_url( $primary_tag_url ); ?>"><?php echo esc_html( $primary_tag ); ?></a>
+  <?php endif; ?>
